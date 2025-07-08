b50514b682fdc0d87d1e91fa7591c9859f67a562
gogy4 <younggogy@gmail.com>
2025-07-08 15:44:27 +0500

    добавил апи сервисы

--- HackUbrir/Infrastructure/Data/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace HackUbrir.Infrastructure.Data;
-
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
-{
-    
-}
--- HackUbrir/obj/Debug/net8.0/HackUbrir.AssemblyInfo.cs
+++ HackUbrir/obj/Debug/net8.0/HackUbrir.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackUbrir")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5e29016d474525bad4405dd2b53405a0e7c3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97446df6663856e662abd281bed45759dcebff15")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackUbrir")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackUbrir")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
